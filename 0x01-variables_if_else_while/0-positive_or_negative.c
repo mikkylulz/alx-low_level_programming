6a0c117a0e4bb97fe215d838f7eef6e1b403ce2e
Adeogun Michael <softworld17@gmail.com>
2022-09-11 04:37:12 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* description: A program that will execute random number each time it
+* is executed
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+
+	if (n < 0)
+	{
+	printf(" %d is zero\n", n);
+	}
+
+	if (n == 0)
+	{
+	printf(" %d is negative\n", n);
+	}
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/** 
+* main - Entry point here
+*
+* description: Write a program that prints the alphabet
+*
+* Return: Return 0 for success 
+*/
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+
+return (0);
+}
