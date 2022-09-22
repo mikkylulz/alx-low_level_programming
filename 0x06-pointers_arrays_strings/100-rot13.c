0291c3b79f55330bbb3c3123972edef8157e57c4
Adeogun Michael <softworld17@gmail.com>
2022-09-22 16:10:50 -0700

    clear
    git commit -m clear

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,15 +8,20 @@
 */
 char *rot13(char *s)
 {
+
 int i, j;
 char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
 for (i = 0; *(s + i); i++)
 {
+
 for (j = 0; j < 52; j++)
 {
+
 if (a[j] == *(s + i))
 {
+
 *(s + i) = b[j];
 break;
 }
