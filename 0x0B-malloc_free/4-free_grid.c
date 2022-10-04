28ec872b7984e37386a63479c62a0a22b6dcb3a6
Adeogun Michael <softworld17@gmail.com>
2022-10-04 13:09:24 -0700

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,12 +13,12 @@
 void free_grid(int **grid, int height)
 {
 if (grid != NULL && height != 0)
-	{
-		while (height >= 0)
-		{
-			free(grid[height]);
-			height--;
-		}
-		free(grid);
-	}
+{
+while (height >= 0)
+{
+free(grid[height]);
+height--;
+}
+free(grid);
+}
 }
